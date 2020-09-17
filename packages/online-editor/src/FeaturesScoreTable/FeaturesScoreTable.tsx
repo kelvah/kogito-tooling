7bed3e09d9117cda51e35d2a2fd57f9890c6b418
roberto emanuel <kelvah@gmail.com>
2020-09-17 18:45:59 +0200

    DAAS-POC Added explanation section

---
+++ packages/online-editor/src/FeaturesScoreTable/FeaturesScoreTable.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import { DataList, DataListCell, DataListItem, DataListItemCells, DataListItemRow } from "@patternfly/react-core";
+import { FeatureScores, formattedScore } from "../FeaturesScoreChart/FeaturesScoreChart";
+import "./FeaturesScoreTable.scss";
+
+interface FeatureScoreTableProps {
+  featuresScore: FeatureScores[];
+}
+
+const FeaturesScoreTable = (props: FeatureScoreTableProps) => {
+  const { featuresScore } = props;
+  const positiveScores = featuresScore.filter(item => item.featureScore >= 0).reverse();
+  const negativeScores = featuresScore.filter(item => item.featureScore < 0).reverse();
+
+  return (
+    <section className="feature-score-table">
+      {positiveScores && <ScoreTable name="Positive Weight" featuresScore={positiveScores} />}
+      {negativeScores && <ScoreTable name="Negative Weight" featuresScore={negativeScores} />}
+    </section>
+  );
+};
+
+interface ScoreTableProps {
+  name: string;
+  featuresScore: FeatureScores[];
+}
+
+const ScoreTable = (props: ScoreTableProps) => {
+  const { name, featuresScore } = props;
+  return (
+    <DataList aria-label="Features Scores" className="pf-m-compact score-table">
+      <DataListItem aria-labelledby="scores" className="score-table__heading">
+        <DataListItemRow>
+          <DataListItemCells
+            dataListCells={[
+              <DataListCell key="primary heading" width={2}>
+                <strong>{name}</strong>
+              </DataListCell>,
+              <DataListCell key="secondary heading">
+                <strong>Score</strong>
+              </DataListCell>
+            ]}
+          />
+        </DataListItemRow>
+      </DataListItem>
+      {featuresScore.map(item => (
+        <DataListItem key={item.featureName} aria-labelledby={`feature-${item.featureName.replace(" ", "-")}`}>
+          <DataListItemRow>
+            <DataListItemCells
+              dataListCells={[
+                <DataListCell key="feature-name" width={2}>
+                  <span id="simple-item2">{item.featureName}</span>
+                </DataListCell>,
+                <DataListCell key="feature-score">{formattedScore(item.featureScore)}</DataListCell>
+              ]}
+            />
+          </DataListItemRow>
+        </DataListItem>
+      ))}
+    </DataList>
+  );
+};
+
+export default FeaturesScoreTable;
--- packages/online-editor/src/editor/OutputViewer/OutputViewer.tsx
+++ packages/online-editor/src/editor/OutputViewer/OutputViewer.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
-import { EvaluationStatus, ResponseMessage, ResponsePayload } from "../ModelTester/ModelTester";
+import { useEffect, useState } from "react";
+import { DecisionResult, EvaluationStatus, ResponseMessage, ResponsePayload } from "../ModelTester/ModelTester";
 import { v4 as uuid } from "uuid";
 import {
   Alert,
   AlertProps,
+  Button,
   Card,
   CardTitle,
   CardBody,
@@ -12,67 +14,203 @@ import {
   FlexItem,
   Label,
   LabelProps,
+  Grid,
+  GridItem,
   Stack,
   StackItem,
-  Title
+  Title,
+  Tooltip
 } from "@patternfly/react-core";
-import { CheckCircleIcon, WarningTriangleIcon, InfoCircleIcon } from "@patternfly/react-icons";
+import {
+  ArrowRightIcon,
+  ArrowLeftIcon,
+  CheckCircleIcon,
+  WarningTriangleIcon,
+  InfoCircleIcon,
+  OutlinedQuestionCircleIcon
+} from "@patternfly/react-icons";
 import ResponseViewer, { isObjectOrArrayOfObjects, ObjectProperty } from "../ResponseViewer/ResponseViewer";
 import "./OutputViewer.scss";
+import FeaturesScoreChart, { FeatureScores } from "../../FeaturesScoreChart/FeaturesScoreChart";
+import FeaturesScoreTable from "../../FeaturesScoreTable/FeaturesScoreTable";
 
 interface OutputViewerProps {
   responsePayload: ResponsePayload;
 }
 
 const OutputViewer = ({ responsePayload }: OutputViewerProps) => {
+  const [viewSection, setViewSection] = useState(1);
+  const [decisionDetail, setDecisionDetail] = useState<DecisionResult | null>(null);
+
+  const viewDecision = (decisionId: string) => {
+    const decision = responsePayload.decisionResults.find(item => item.decisionId === decisionId);
+    if (decision) {
+      setDecisionDetail(decision);
+      setViewSection(2);
+    }
+  };
+  const backToResults = () => {
+    setViewSection(1);
+    setTimeout(() => {
+      setDecisionDetail(null);
+    }, 300);
+  };
+  useEffect(() => {
+    setViewSection(1);
+    setDecisionDetail(null);
+  }, [responsePayload]);
+
+  const featureScores = [
+    {
+      featureName: "Age",
+      featureScore: -0.08937896629080377
+    },
+    {
+      featureName: "State",
+      featureScore: 0.21272743757961554
+    },
+    {
+      featureName: "Points",
+      featureScore: 0.23272743757961554
+    },
+    {
+      featureName: "City",
+      featureScore: 0.6780527129423648
+    },
+    {
+      featureName: "Code",
+      featureScore: 0.7033802543201365
+    },
+    {
+      featureName: "Speed Limit",
+      featureScore: 0.7693802543201365
+    },
+    {
+      featureName: "Actual Speed",
+      featureScore: -0.9240811677386516
+    }
+  ] as FeatureScores[];
+
   return (
     <section className="output-viewer">
-      <div className="output-viewer__section">
-        <Title headingLevel="h4" className="output-viewer__section__title">
-          Decision Results
-        </Title>
-        <Stack hasGutter={true}>
-          {responsePayload.decisionResults &&
-            responsePayload.decisionResults.map(decision => {
-              return (
-                <StackItem key={decision.decisionId}>
-                  <Card isCompact={true} isHoverable={true}>
-                    <CardTitle>{decision.decisionName}</CardTitle>
-                    <CardBody>
-                      {isObjectOrArrayOfObjects(decision.result) && (
-                        <ResponseViewer source={decision.result as object} />
-                      )}
-                      {!isObjectOrArrayOfObjects(decision.result) && (
-                        <ObjectProperty property="" value={decision.result} />
-                      )}
-                    </CardBody>
-                    <CardFooter>
-                      <Flex>
-                        <FlexItem align={{ default: "alignRight" }}>
-                          <EvaluationStatusLabel status={decision.evaluationStatus} />
-                        </FlexItem>
-                      </Flex>
-                    </CardFooter>
-                  </Card>
-                </StackItem>
-              );
-            })}
-        </Stack>
-      </div>
-      <div className="output-viewer__section">
-        <Title headingLevel="h4" className="output-viewer__section__title">
-          Messages
-        </Title>
-        <Stack hasGutter={true}>
-          {responsePayload.messages &&
-            responsePayload.messages.map(message => {
-              return (
-                <StackItem key={uuid()}>
-                  <MessageAlert message={message} />
-                </StackItem>
-              );
-            })}
-        </Stack>
+      <div className={`output-viewer__content ${viewSection === 2 ? "output-viewer__content--second-panel" : ""}`}>
+        <Grid>
+          <GridItem span={6}>
+            <div className="output-viewer__content__panel">
+              <div className="output-viewer__section">
+                <Title headingLevel="h4" className="output-viewer__section__title">
+                  Decision Results
+                </Title>
+                <Stack hasGutter={true}>
+                  {responsePayload.decisionResults &&
+                    responsePayload.decisionResults.map(decision => {
+                      return (
+                        <StackItem key={decision.decisionId}>
+                          <Card isCompact={true}>
+                            <CardTitle>{decision.decisionName}</CardTitle>
+                            <CardBody>
+                              {isObjectOrArrayOfObjects(decision.result) && (
+                                <ResponseViewer source={decision.result as object} />
+                              )}
+                              {!isObjectOrArrayOfObjects(decision.result) && (
+                                <ObjectProperty property="" value={decision.result} />
+                              )}
+                            </CardBody>
+                            <CardFooter>
+                              <Flex>
+                                <FlexItem align={{ default: "alignLeft" }}>
+                                  <em>Evaluation Status:</em>{" "}
+                                  <EvaluationStatusLabel status={decision.evaluationStatus} />
+                                </FlexItem>
+                                <FlexItem align={{ default: "alignRight" }}>
+                                  <Button
+                                    variant="link"
+                                    icon={<ArrowRightIcon />}
+                                    iconPosition="right"
+                                    onClick={() => viewDecision(decision.decisionId)}
+                                  >
+                                    View Explanation
+                                  </Button>
+                                </FlexItem>
+                              </Flex>
+                            </CardFooter>
+                          </Card>
+                        </StackItem>
+                      );
+                    })}
+                </Stack>
+              </div>
+              <div className="output-viewer__section">
+                <Title headingLevel="h4" className="output-viewer__section__title">
+                  Messages
+                </Title>
+                <Stack hasGutter={true}>
+                  {responsePayload.messages &&
+                    responsePayload.messages.map(message => {
+                      return (
+                        <StackItem key={uuid()}>
+                          <MessageAlert message={message} />
+                        </StackItem>
+                      );
+                    })}
+                </Stack>
+              </div>
+            </div>
+          </GridItem>
+          <GridItem span={6}>
+            <div className="output-viewer__content__panel">
+              {decisionDetail && (
+                <>
+                  <Button variant="link" icon={<ArrowLeftIcon />} iconPosition="left" onClick={backToResults}>
+                    Back to Results
+                  </Button>
+                  <Stack hasGutter={true}>
+                    <StackItem>
+                      <Card isCompact={true} isFlat={true}>
+                        <CardTitle>{decisionDetail.decisionName}</CardTitle>
+                        <CardBody>
+                          {isObjectOrArrayOfObjects(decisionDetail.result) && (
+                            <ResponseViewer source={decisionDetail.result as object} />
+                          )}
+                          {!isObjectOrArrayOfObjects(decisionDetail.result) && (
+                            <ObjectProperty property="" value={decisionDetail.result} />
+                          )}
+                        </CardBody>
+                      </Card>
+                    </StackItem>
+                    <StackItem>
+                      <Card isCompact={true} isFlat={true}>
+                        <CardTitle>
+                          Explanation Chart{" "}
+                          <Tooltip
+                            position="top"
+                            content={
+                              <div>
+                                The explanation chart displays each input influencing the decision result, with a
+                                positive or negative score.
+                              </div>
+                            }
+                          >
+                            <OutlinedQuestionCircleIcon style={{ color: "var(--pf-global--primary-color--100)" }} />
+                          </Tooltip>
+                        </CardTitle>
+                        <FeaturesScoreChart featuresScore={featureScores} />
+                      </Card>
+                    </StackItem>
+                    <StackItem>
+                      <Card isCompact={true} isFlat={true}>
+                        <CardTitle>Explanation Scores</CardTitle>
+                        <CardBody>
+                          <FeaturesScoreTable featuresScore={featureScores} />
+                        </CardBody>
+                      </Card>
+                    </StackItem>
+                  </Stack>
+                </>
+              )}
+            </div>
+          </GridItem>
+        </Grid>
       </div>
     </section>
   );
